f84e6a2202b2023c96117646d98b29d5c8de8454
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-09-23 16:07:37 +0100

    Added balance limit & method feedback

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,18 @@
 class Oystercard
 
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    @balance += money
+    if @balance + money > MAXIMUM_BALANCE
+      "Balance limit exceeded. Please top up #{MAXIMUM_BALANCE - @balance} or less"
+    else
+      "Card balance: #{@balance += money}"
+    end
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -16,8 +16,16 @@ describe Oystercard do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
 
+    it 'returns card balance after topping up' do
+      expect(subject.top_up(20)).to match("Card balance: #{subject.balance}")
+    end
+
     it 'expect oystercard balance to change by 5' do
       expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+
+    it 'tells user balance limit exceeded' do
+      expect(subject.top_up(95)).to match("Balance limit exceeded. Please top up #{Oystercard::MAXIMUM_BALANCE - subject.balance} or less")
     end
 
   end
