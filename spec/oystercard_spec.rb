311a5735b4d34b4e2eecf50bc22ef46ac455345e
Dom Tunstill <datunstill@gmail.com>
2019-09-23 15:36:54 +0100

    Added top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,21 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  describe '#top_up' do
 
+    it 'expect oystercard to respond to top_up method' do
+      expect(subject).to respond_to(:top_up)
+    end
+
+    it 'expect oystercard to respond to top_up method' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'expect oystercard balance to change by 5' do
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+
+  end
 
 
 
