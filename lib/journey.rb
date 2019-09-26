913cf8989f545dfc0aae1ffe76684c48f0dc0ceb
Melvin Lau <lau.melvin@icloud.com>
2019-09-26 17:47:33 +0100

    Step 14, completed default for no exit station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,20 +1,26 @@
 class Journey
 
-  attr_reader :entry, :exit, :fare
+  attr_reader :entry, :exit
   @@history = []
+  DEFAULT_ACTION = "no info"
+  MINIMUM_FARE = 1.00
+  PENALTY_FARE = 6.00
 
   def self.history
     @@history
   end
 
-  def initialize(entry, exit, fare)
+  def fare
+    return PENALTY_FARE if @exit == DEFAULT_ACTION
+    MINIMUM_FARE
+  end
+
+  def initialize(entry, exit=DEFAULT_ACTION)
     @entry = entry
     @exit = exit
-    @fare = fare
-
   end
 
   def update_history
-    @@history << { entry: @entry, exit: @exit, fare: @fare }
+    @@history << { entry: @entry, exit: @exit }
   end
 end
