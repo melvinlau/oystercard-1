ae7adc5f7a48198a9b4d6fb84ddff1cc8634acb7
Melvin Lau <lau.melvin@icloud.com>
2019-09-26 15:56:51 +0100

    Step 14: wrote test for update_history with bugs

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,4 +1,12 @@
 class Journey
-
-
+  attr_reader :entry, :exit, :fare, :history
+  def initialize(entry, exit, fare)
+    @entry = entry
+    @exit = exit
+    @fare = fare
+    @history = []
+  end
+  def log
+    { entry: @entry, exit: @exit, fare: @fare }
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,9 +1,39 @@
 require 'journey'
 
 describe Journey do
+  let(:journey) { described_class.new("entry", "exit", 1.00) }
+
+  it "initializes with an empty history" do
+    expect(journey.history).to eq []
+  end
+
   describe '#log' do
     it 'logs most recent completed journeys' do
-
+      expect(journey.log).to eq({ entry: "entry", exit: "exit", fare: 1.00 })
     end
   end
+
+  describe "#update_history" do
+    it "pushes the latest complete journey log into the journey history" do
+
+      # Previous journey
+      allow(journey).to receive(:history) { [ {"aldgate", "liverpool street", 1.00} ] }
+
+
+
+      # Current journey
+      journey_two = Journey.new("bank", "temple", 1.00)
+      journey.log
+
+      expected_history = [
+        {entry: "aldgate", exit: "liverpool street", 1.00},
+        {entry: "bank", exit: "temple", 1.00}
+      ]
+
+      expect(journey.update_history).to eq expected_history
+    end
+  end
+
+
+
 end
