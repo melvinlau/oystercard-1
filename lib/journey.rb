87652af76030b4d90c45f9947188140115d18545
Vijay Kurian <kurianvijay7@gmail.com>
2019-09-26 17:04:45 +0100

    Added journey class and test passing for update journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,12 +1,20 @@
 class Journey
-  attr_reader :entry, :exit, :fare, :history
+
+  attr_reader :entry, :exit, :fare
+  @@history = []
+
+  def self.history
+    @@history
+  end
+
   def initialize(entry, exit, fare)
     @entry = entry
     @exit = exit
     @fare = fare
-    @history = []
+
   end
-  def log
-    { entry: @entry, exit: @exit, fare: @fare }
+
+  def update_history
+    @@history << { entry: @entry, exit: @exit, fare: @fare }
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,36 +2,35 @@ require 'journey'
 
 describe Journey do
   let(:journey) { described_class.new("entry", "exit", 1.00) }
+  let(:journey2) {described_class.new("bank", "temple", 1.00)}
 
   it "initializes with an empty history" do
-    expect(journey.history).to eq []
+    expect(described_class.history).to eq []
   end
 
-  describe '#log' do
-    it 'logs most recent completed journeys' do
-      expect(journey.log).to eq({ entry: "entry", exit: "exit", fare: 1.00 })
-    end
-  end
+  # describe '#log' do
+  #   it 'logs most recent completed journeys' do
+  #     expect(journey.log).to eq({ entry: "entry", exit: "exit", fare: 1.00 })
+  #   end
+  # end
 
   describe "#update_history" do
-    it "pushes the latest complete journey log into the journey history" do
+    it "pushes the first complete journey log into the journey history" do
 
-      # Previous journey
-      allow(journey).to receive(:history) { [ {"aldgate", "liverpool street", 1.00} ] }
-
-
-
-      # Current journey
-      journey_two = Journey.new("bank", "temple", 1.00)
-      journey.log
-
-      expected_history = [
-        {entry: "aldgate", exit: "liverpool street", 1.00},
-        {entry: "bank", exit: "temple", 1.00}
-      ]
-
-      expect(journey.update_history).to eq expected_history
+      expected_outcome = [{entry: "entry", exit: "exit", fare: 1.00 }]
+      # expect(journey.update_history).to eq Journey.history
+      expect(journey.update_history).to eq expected_outcome
     end
+
+    # it "pushes subsequent complete journies into the journey history" do
+    # ()
+    #   journey.log
+    #   journey.update_history
+    #   journey2.log
+    #   expected_outcome = [ {entry: "entry", exit: "exit", fare: 1.00 },
+    #     {entry: "bank", exit: "temple", fare: 1.00 } ]
+    #   expect(journey2.update_history).to eq expected_outcome
+    # end
   end
 
 
